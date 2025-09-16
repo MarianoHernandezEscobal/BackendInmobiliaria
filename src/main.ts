48eeeb0d3c6f7bbbdcdf993cb94d66e5a80db5e8
Mariano Hernandez <mariano.escobal@cencosud.com.uy>
2025-09-16 17:13:19 -0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
   const port = configService.get<number>('PORT') || 10000;
   const env = configService.get<string>('NODE_ENV') || 'development';
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()} in ${env} mode`);
 }
 
